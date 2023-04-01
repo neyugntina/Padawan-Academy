fa050f1e567be3dceec8dbe89d082d84470f5437
Maria Doan <mariadoan247@ou.edu>
2023-04-01 14:42:35 -0500

    Created base homepage

--- Padawan Academy/ContentView.swift
+++ Padawan Academy/ContentView.swift
@@ -1,88 +1,77 @@
-//
-//  ContentView.swift
-//  Padawan Academy
-//
-//  Created by Tina Nguyen on 4/1/23.
-//
-
 import SwiftUI
-import CoreData
+
+struct Course: Identifiable {
+    let id = UUID()
+    let name: String
+}
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var courses: [Course] = [
+        Course(name: "Math"),
+        Course(name: "Science"),
+        Course(name: "English")
+    ]
+    
+    @State var showAddCourse = false
+    @State var newCourseName = ""
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                Spacer()
+                
+                Text("Courses")
+                    .font(.largeTitle)
+                    .padding(.top, 20)
+                
+                Spacer()
+                
+                List {
+                    ForEach(courses) { course in
+                        Text(course.name)
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                
+                Button(action: {
+                    self.showAddCourse = true
+                }) {
+                    Image(systemName: "plus")
+                        .padding()
+                        .foregroundColor(.white)
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                .background(Color.blue)
+                .clipShape(Circle())
+                .padding(.trailing, 20)
+                .padding(.bottom, 20)
+                .shadow(radius: 10)
+            }
+            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
+            .sheet(isPresented: $showAddCourse) {
+                VStack {
+                    TextField("New course name", text: self.$newCourseName)
+                        .padding()
+                    
+                    Button(action: {
+                        self.courses.append(Course(name: self.newCourseName))
+                        self.showAddCourse = false
+                        self.newCourseName = ""
+                    }) {
+                        Text("Add")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
                     }
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                .padding()
             }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
