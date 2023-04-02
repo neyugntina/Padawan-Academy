ebbc6026c5c1b691242e7d072f9a23fde519c5a7
Maria Doan <mariadoan247@ou.edu>
2023-04-01 21:55:53 -0500

    Pulled delete function from Ted's branch

--- Padawan Academy/Courses.swift
+++ Padawan Academy/Courses.swift
@@ -9,65 +9,73 @@ import SwiftUI
 
 struct Courses: View {
     @State var courses: [Course] = [
-        Course(name: "Math"),
-        Course(name: "Science"),
-        Course(name: "English")
-    ]
-    
-    @State var showAddCourse = false
-    @State var newCourseName = ""
-    var body: some View {
-        NavigationView {
-            VStack {
-                HStack {
+            Course(name: "Math"),
+            Course(name: "Science"),
+            Course(name: "English")
+        ]
+        
+        @State var showAddCourse = false
+        @State var newCourseName = ""
+        
+        var body: some View {
+            NavigationView {
+                VStack {
+                    Spacer()
+                    
                     Text("Courses")
                         .font(.largeTitle)
-                }
-                
-                Spacer()
-                
-                List {
-                    ForEach(courses) { course in
-                        Text(course.name)
+                        .padding(.top, 20)
+                    
+                    Spacer()
+                    
+                    List {
+                        ForEach(courses) { course in
+                            Text(course.name)
+                        }
+                        .onDelete(perform: delete)
+
                     }
-                }
-                
-                Button(action: {
-                    self.showAddCourse = true
-                }) {
-                    Image(systemName: "plus")
-                        .padding()
-                        .foregroundColor(.white)
-                }
-                .background(Color.blue)
-                .clipShape(Circle())
-                .padding(.trailing, 20)
-                .padding(.bottom, 20)
-                .shadow(radius: 10)
-            }
-            .navigationBarTitle("", displayMode: .inline)
-            .navigationBarHidden(true)
-            .sheet(isPresented: $showAddCourse) {
-                VStack {
-                    TextField("New course name", text: self.$newCourseName)
-                        .padding()
                     
                     Button(action: {
-                        self.courses.append(Course(name: self.newCourseName))
-                        self.showAddCourse = false
-                        self.newCourseName = ""
+                        self.showAddCourse = true
                     }) {
-                        Text("Add")
+                        Image(systemName: "plus")
                             .padding()
-                            .background(Color.blue)
                             .foregroundColor(.white)
-                            .cornerRadius(10)
                     }
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .padding(.trailing, 20)
+                    .padding(.bottom, 20)
+                    .shadow(radius: 10)
                 }
-                .padding()
+                .navigationBarTitle("", displayMode: .inline)
+                .navigationBarHidden(true)
+                .sheet(isPresented: $showAddCourse) {
+                    VStack {
+                        TextField("New course name", text: self.$newCourseName)
+                            .padding()
+                        
+                        Button(action: {
+                            self.courses.append(Course(name: self.newCourseName))
+                            self.showAddCourse = false
+                            self.newCourseName = ""
+                        }) {
+                            Text("Add")
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                    .padding()
+                }
             }
         }
-    }
+        
+        func delete(at offsets: IndexSet) {
+            courses.remove(atOffsets: offsets)
+        }
 }
 
 struct Courses_Previews: PreviewProvider {
